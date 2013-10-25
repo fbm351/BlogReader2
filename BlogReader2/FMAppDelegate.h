eb187145b842fd92bc36aa5aad7e1c835ca372a0
Fred Myers <fred@fred-myers.com>
2013-10-25 16:48:34 -0400

    Initial Commit

---
+++ BlogReader2/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  BlogReader2
+//
+//  Created by Fredrick Myers on 10/25/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader2
+//
+//  Created by Fredrick Myers on 10/25/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMAppDelegate class]));
+    }
+}
