749e40ca7d241a404784f3af4e299e8cdab09817
Fredrick Myers <fmyers@fredricks-mbp.gateway.2wire.net>
2013-10-26 14:07:42 -0400

    Added TableViewController

---
+++ BlogReader2/FMTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  FMTableViewController.h
+//  BlogReader2
+//
+//  Created by Fredrick Myers on 10/26/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMTableViewController : UITableViewController
+@property (nonatomic, strong) NSArray *titles;
+
+@end
---
+++ BlogReader2/FMTableViewController.m
@@ -0,0 +1,121 @@
+//
+//  FMTableViewController.m
+//  BlogReader2
+//
+//  Created by Fredrick Myers on 10/26/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import "FMTableViewController.h"
+
+@interface FMTableViewController ()
+
+@end
+
+@implementation FMTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.titles = [NSArray arrayWithObjects:@"Getting Started with WordPress",
+                   @"Whitespace in Web Design: What It Is and Why You Should Use It",
+                   @"Adaptive Images and Responsive SVGs - Treehouse Show Episode 15",
+                   @"Productivity is About Contraints and Concentration",
+                   @"A Guide to Becoming the Smartest Developer on the Planet",
+                   @"Teacher Spotlight: Zac Gordon",
+                   @"Do You Love What You Do?",
+                   @"Applying Normalize.css Reset - Quick Tip",
+                   @"How I Wrote a Book in 3 Days",
+                   @"Responsive Techniques, JavaScript MVC Frameworks, Firefox 16 | Treehouse Show Episode 14", nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.titles.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    cell.textLabel.text = [self.titles objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
